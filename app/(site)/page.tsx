502bfed14f0d2953fad721207ea45090e21857a4
USER <user@USERs-MacBook-Pro.local>
2023-08-27 01:11:01 +0545

    Header Items and hover effect on item play button

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 import React from "react";
 
 function page() {
@@ -7,9 +8,20 @@ function page() {
       <Header>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back!</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4"></div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem
+              image={"/images/liked.png"}
+              name={"Liked Songs"}
+              href={""}
+            />
+          </div>
         </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex items-center justify-between">
+          <h1 className="text-white text-2xl font-semibold">Newest Songs</h1>
+        </div>
+      </div>
     </div>
   );
 }
