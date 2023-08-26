74f7da8c35bd74ef9755fea538c9402cad8aa936
USER <user@USERs-MacBook-Pro.local>
2023-08-26 23:48:53 +0545

    Sidebar Completes

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function page() {
-  return <div className="text-green-500">Main Content</div>;
+  return <div className="flex text-green-500">Main Content</div>;
 }
 
 export default page;
